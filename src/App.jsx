8c8212c3aaf49ebfc90fda5a7086dba5dfdac0f8
IvanLowLLH <ivanlowlh@gmail.com>
2025-12-29 12:11:03 +0800

    Basic set-up for game

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [gameInfo, setGameInfo] = useState({
+    cardIds: [],
+    score: 0,
+    bestScore: 0,
+  });
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="header">
+        <div className="title">
+          <h1>Pokemon Game</h1>
+        </div>
+        <div className="score-board">
+          <p>Score: {gameInfo.score}</p>
+          <p>Best Score: {gameInfo.bestScore}</p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="game-board"></div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
